377294dd95917492274b54f6144e60cf83f1a3e3
piekario13 <jpiekarski@jpiekarski-nm-1.home>
2021-05-02 12:12:59 +0200

    step1

---
+++ src/components/Post.js
@@ -0,0 +1,18 @@
+import React from  'react';
+import '../styles//Post.css';
+
+const Post = () => {
+    return (
+        <div className="divPost">
+            <div className="userDiv">
+                <h5 id="1">Imię Nazwisko</h5>
+                <div className="user"></div>
+            </div>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ullam debitis nisi doloremque magnam repellat excepturi aliquid similique, dolores mollitia, ipsum nobis nostrum, laborum aspernatur expedita nihil commodi culpa inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit.  </p>
+        </div>
+    )
+}
+
+
+export default Post
